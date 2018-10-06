b398a361fb1d6992ba1dc1e6d6a72d886894a939
Yuto Tachibana <tachibanayu24@gmail.com>
2018-10-06 21:11:47 +0900

    create frame of app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+// import Button from '@material-ui/core/Button';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="header">
+          <h3>Yuto Tachibana's Portfolio</h3>
+        </div>
+        <div className="main-content">
+          <div className="sub-content bio">
+            <span className="content-title">bio</span>
+          </div>
+          <div className="sub-content skill">
+            <span className="content-title">skill</span>
+          </div>
+          <div className="sub-content work">
+            <span className="content-title">works</span>
+          </div>
+          <div className="sub-content photo">
+            <span className="content-title">photo</span>
+          </div>
+          <div className="sub-content sns">
+            <span className="content-title">sns</span>
+          </div>
+        </div>
+        <div className="footer">
+          <p>copyright 2018- Yuto Tachibana</p>
+        </div>
       </div>
     );
   }
