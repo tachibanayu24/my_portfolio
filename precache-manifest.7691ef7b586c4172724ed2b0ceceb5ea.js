d006b64a4d0b9ab1be626f97c1a67c362c3c2ef7
Yuto Tachibana <tachibanayu24@gmail.com>
2018-10-07 00:47:25 +0900

    Updates

---
+++ precache-manifest.7691ef7b586c4172724ed2b0ceceb5ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2bb3e8d5477afa4b7c6",
+    "url": "/my-portfolio/static/js/runtime~main.f2bb3e8d.js"
+  },
+  {
+    "revision": "96e7951caef4a2106351",
+    "url": "/my-portfolio/static/js/main.96e7951c.chunk.js"
+  },
+  {
+    "revision": "023d4072aff8375d48e7",
+    "url": "/my-portfolio/static/js/1.023d4072.chunk.js"
+  },
+  {
+    "revision": "96e7951caef4a2106351",
+    "url": "/my-portfolio/static/css/main.c73a09f1.chunk.css"
+  },
+  {
+    "revision": "b9e3f3cac5a05ff05afe03501ec2b143",
+    "url": "/my-portfolio/index.html"
+  }
+];
