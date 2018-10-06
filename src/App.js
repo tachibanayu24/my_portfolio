0f8989b1eaf9a5568141447f0d31ffb7a298c819
Yuto Tachibana <tachibanayu24@gmail.com>
2018-10-07 00:37:53 +0900

    implement some icons

--- src/App.js
+++ src/App.js
@@ -17,43 +17,67 @@ import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import Divider from '@material-ui/core/Divider';
 
+import FaceIcon from '@material-ui/icons/FaceTwoTone';
+import CodeIcon from '@material-ui/icons/CodeTwoTone';
+import BrushIcon from '@material-ui/icons/BrushTwoTone';
+import CameraIcon from '@material-ui/icons/PhotoCameraTwoTone';
+import WhatshotIcon from '@material-ui/icons/WhatshotTwoTone';
+
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <AppBar position="static">
+        <AppBar position="static" color="default">
         <Toolbar>
           <Typography variant="title" color="inherit" style={{ fontFamily: "Courier New", fontWeight: 900}} noWrap>
-            Yuto Tachibana's Portfolio
+            <span style={{color: '#1E90FF' }}>/Yuto_Tachibana</span> <span style={{ color: '#FF6347' }}>❯</span><span style={{ color: '#A9A9A9'}}>❯❯ Portfolio_</span>
           </Typography>
         </Toolbar>
         </AppBar>
 
         <main>
           <div className="sub-content bio">
-            <span className="content-title">bio</span>
+            <div className="content-title">
+              <FaceIcon style={{height: 35, width: 35, marginRight: 10,}} />
+              <span>bio</span>
+            </div>
             <Divider inset />
             <div className="sub-content-body">
-              <p>
-                都内在住のテック好きな男子です。<br />
-                家庭の事情で高校を1年で中退して肉体労働者になりましたが、お金をためて高卒認定を取得し、都内の大学の工学部へ進学。<br />
-                大学に通いながら新宿のベンチャー企業で事務、業務改善、採用活動、ビジネス開発とマルチに働いていました。<br />
-                2018年4月に港区の大手通信系SIerに新卒入社し、そこではビジネス企画、研究開発系のお仕事をしています。
-              </p>
+              <p>都内在住のテック好きな男子です。</p>
+              <p>家庭の事情で高校を1年で中退して肉体労働者になりましたが、お金をためて高卒認定を取得し、都内の大学(工学部)へ進学。<br />
+                大学に通いながら新宿のベンチャー企業でサービス運用、業務改善、採用活動、ビジネス開発とマルチに働いていました。</p>
+              <p>2018年4月に港区の大手通信系SIerに新卒入社し、そこではビジネス企画、研究開発系のお仕事をしています。<br />
+                業務ではあまりコードを書く機会を作れないので、プライベートでよくコーディングしています。</p>
             </div>
           </div>
           <div className="sub-content skill">
-            <span className="content-title">skill</span>
+            <div className="content-title">
+              <CodeIcon style={{height: 35, width: 35, marginRight: 10,}} />
+              <span>skills</span>
+            </div>
+            <Divider inset />
           </div>
           <div className="sub-content work">
-            <span className="content-title">works</span>
+            <div className="content-title">
+              <BrushIcon style={{height: 35, width: 35, marginRight: 10,}} />
+              <span>works/activities</span>
+              <Divider inset />
+            </div>
           </div>
           <div className="sub-content photo">
-            <span className="content-title">photo</span>
+            <div className="content-title">
+              <CameraIcon style={{height: 35, width: 35, marginRight: 10,}} />
+              <span>photos</span>
+              <Divider inset />
+            </div>
           </div>
           <div className="sub-content sns">
-            <span className="content-title">sns</span>
+            <div className="content-title">
+              <WhatshotIcon style={{height: 35, width: 35, marginRight: 10,}} />
+              <span>sns</span>
+              <Divider inset />
+            </div>
           </div>
         </main>
 
