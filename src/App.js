51046dc6102d86501b0271919244ac6a7ba8a436
Yuto Tachibana <tachibanayu24@gmail.com>
2018-10-10 21:13:51 +0900

    add last-modified date

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
       <React.Fragment>
         <AppBar position="sticky" style={{backgroundColor: '#022B36E0'}}>
           <Toolbar>
-            <Typography variant="title" color="inherit" style={{ fontSize: 18, fontFamily: 'Monaco', fontWeight: 900}} noWrap>
+            <Typography variant="title" color="inherit" style={{ fontSize: 18, fontFamily: 'Monaco', fontWeight: 500}} noWrap>
               <span style={{color: '#1E90FF' }}>~/YutoTachibana</span> <span style={{ color: '#FF6347' }}>❯</span><span style={{ color: '#A9A9A9'}}>❯❯ Portfolio<span className="blink">_</span></span>
             </Typography>
           </Toolbar>
@@ -232,7 +232,7 @@ class App extends Component {
         </main>
 
         <footer>
-          <p>copyright 2018- Yuto Tachibana</p>
+          <p>Last-Modified: 2018/10/10</p>
         </footer>
     </React.Fragment>
     );
